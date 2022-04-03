8fe47bcfe66ee9d0c3eb32cabc6e6b8a271c139f
Cedro <yotechgirl@gmail.com>
2022-04-03 09:59:10 -0300

    feat:challenge 4 ok

--- scripts/chalenge-3.js
+++ scripts/chalenge-3.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Flamengo Champios 2022';
+// console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,9 +13,9 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['Flamengo', 'Vasco', 'Gremio', 'Fluminense', 'Santos'];
 
-console.log('Times que estão participando do campeonato:', teams);
+// console.log('Times que estão participando do campeonato:', teams);
 
 /*
 Crie uma função chamada `showTeamPosition` com as seguintes características:
@@ -32,19 +33,34 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(number) {
+  return number > 5
+    ? 'Não temos a informação do time que está nessa posição'
+    : `O time que está em ${number}º lugar é o ${teams[number]}`;
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log(showTeamPosition(0));
+console.log(showTeamPosition(1));
+console.log(showTeamPosition(2));
+console.log(showTeamPosition(3));
+console.log(showTeamPosition(6));
+
+teams.push('Goias');
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+
+var i = 20;
+while (i <= 30) {
+  console.log(i);
+  i++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +74,52 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(color) {
+  var allCollors = {
+    red: {
+      hex: '#FF69B4',
+    },
+    violet: {
+      hex: '#EE82EE',
+    },
+    purple: {
+      hex: '#A020F0',
+    },
+    orange: {
+      hex: '#FFA500',
+    },
+    yellow: {
+      hex: '#FFFF00',
+    },
+  };
 
-/*
-Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
-*/
-// ?
+  switch (color) {
+    case 'red':
+      console.log(`O hexadecimal para a cor red é ${allCollors.red.hex}.`);
+      break;
+    case 'violet':
+      console.log(
+        `O hexadecimal para a cor violet é ${allCollors.violet.hex}.`,
+      );
+      break;
+    case 'purple':
+      console.log(
+        `O hexadecimal para a cor purple é ${allCollors.purple.hex}.`,
+      );
+      break;
+    case 'orange':
+      console.log(
+        `O hexadecimal para a cor orange é ${allCollors.orange.hex}.`,
+      );
+      break;
+    case 'yellow':
+      console.log(
+        `O hexadecimal para a cor yellow é ${allCollors.yellow.hex}.`,
+      );
+      break;
+    default:
+      console.log(`Não temos o equivalente hexadecimal para ${color}`);
+  }
+
+  return allCollors;
+}
--- scripts/chalenge-4.js
+++ scripts/chalenge-4.js
@@ -1,60 +1,92 @@
-*
+/*
 Crie um array com 5 items (tipos variados).
 */
-// ?
+var arr = [
+  'Carolina',
+  [1, 2, 3],
+  { name: 'Carolina', age: 27 },
+  105,
+  false,
+  ['Ana', true, 27],
+];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+function addItem(item) {
+  arr.push(item);
+  return arr;
+}
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+console.log(arr[5]);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log(`O segundo elemento do segundo array é ${arr[5][1]}.`);
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log(`O primeiro array tem ${arr.length} itens.`);
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log(`O segundo array tem ${arr[5].length} itens`);
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
-console.log( 'Números pares entre 10 e 20:' );
-// ?
+// console.log('Números pares entre 10 e 20:');
 
+var number = 10;
+while (number <= 20) {
+  if (number % 2 == 0) {
+    console.log('Números pares entre 10 e 20:', number);
+  }
+  number++;
+}
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
-console.log( 'Números ímpares entre 10 e 20:' );
-// ?
-
+console.log('Números ímpares entre 10 e 20:');
+var impares = 10;
+while (impares <= 20) {
+  if (impares % 2 != 0) {
+    console.log('Números impares entre 10 e 20:', impares);
+  }
+  impares++;
+}
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
 Só vamos mudar o range:
 - No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
-console.log( 'Números pares entre 100 e 120:' );
-// ?
+// console.log('Números pares entre 100 e 120:');
+let num = 100;
+let numImpares = 111;
+for(num=100;num<=200;num++){
+  if(num % 2 == 0){
+    console.log('Números pares entre 100 e 200:', num);
+  }
+}
 
-console.log( 'Números ímpares entre 111 e 125:' );
-// ?
+for(numImpares=111;numImpares<=125;numImpares++){
+  if(numImpares % 2 != 0){
+    console.log('Números impares entre 111 e 125:', numImpares);
+  }
+}
+
+
